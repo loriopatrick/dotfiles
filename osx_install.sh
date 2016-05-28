2e1dec2358ea2ee73bac4701fc4146259886cb9b
Patrick Lorio <patrick@lorio.me>
2016-05-27 23:21:42 -0700

    Add install script for osx

---
+++ osx_install.sh
@@ -0,0 +1,13 @@
+brew 2> /dev/null
+if [ ! $? -eq 1 ]
+then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    brew upgrade
+fi
+
+brew update
+brew install vim
+brew install git
+brew install node
+brew install rust
